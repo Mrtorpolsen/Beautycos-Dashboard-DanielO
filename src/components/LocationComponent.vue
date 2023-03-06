fed61e72270129f6a11d3a49bb6c249c095b5972
Mrtorpolsen <mrtorpolsen@gmail.com>
2023-03-06 12:56:04 +0100

    updated gitignore

--- src/components/LocationComponent.vue
+++ src/components/LocationComponent.vue
@@ -2,6 +2,18 @@
   <div>yo</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Location } from '@/types/Location'
+
+/*const locationExample: Location = {
+
+}*/
+
+const getLocations = async () => {
+  fetch('https://alarm-dev.beautycos.dk/Api/Location/GetLocations', {
+    method: 'GET'
+  })
+}
+</script>
 
 <style lang="scss"></style>
---
+++ src/types/Location.ts
@@ -0,0 +1,5 @@
+export type Location = {
+  uuid: string
+  name: string
+  lastAlaram: string
+}
