cc6d4c0733596548edeb782da6c47d5ac110ff0b
Mrtorpolsen <mrtorpolsen@gmail.com>
2023-03-06 12:24:26 +0100

    basic setup

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dashboardContainer">
+    <LocationComponent></LocationComponent>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LocationComponent from './LocationComponent.vue'
+</script>
+
+<style lang="scss">
+.dashboardContainer {
+  width: 1024px;
+  background-color: lightgrey;
+  border-radius: 4px;
+  height: 500px;
+}
+</style>
